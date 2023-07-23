d1de6d29ad6a682b8100878201693d5d669e4174
dibesh-shah <joeh5730+github@gmail.com>
2023-07-23 15:38:24 +0545

    department added

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('user/welcome');
@@ -21,12 +12,9 @@ Route::get('/register', function () {
     return view('user/register');
 });
 
-
 Route::view('/login', 'user/login');
-
 Route::view('/admin', 'admin/login');
-
 Route::view('/admin/dashboard', 'admin/dashboard');
-
 Route::view('/admin/approve', 'admin/approve');
 Route::view('/admin/approvedetails', 'admin/approvedetails')->name('approvedetail');
+Route::view('/admin/department', 'admin/department');
